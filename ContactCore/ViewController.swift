726021252bacabb2a234180eb76b917e31d72725
ANI <ani@gm009-4.local>
2017-01-24 21:36:44 -0500

    Edit working, and table is updating.

--- ContactCore/ViewController.swift
+++ ContactCore/ViewController.swift
@@ -8,11 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
     
     var contactArray = [Contact]()
+    
+    @IBOutlet var contactTableView  : UITableView!
+    
+    //MARK: - Interavtivity Methods
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "segueListToEdit" {
+            let indexPath = contactTableView.indexPathForSelectedRow!
+            let currentContact = contactArray[indexPath.row]
+            let destVC = segue.destination as! DetailViewController
+            destVC.currentContact = currentContact
+            contactTableView.deselectRow(at: indexPath, animated: true)
+        }
+    }
+    
+    //MARK: - TableView Methods
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contactArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let currentContact = contactArray[indexPath.row]
+        cell.textLabel!.text = currentContact.lastName! + ", " + currentContact.firstName!
+        cell.detailTextLabel!.text = currentContact.phoneNumber
+        return cell
+    }
 
     //MARK: - Life Cycle Methods
 
@@ -24,6 +52,7 @@ class ViewController: UIViewController {
         super.viewWillAppear(animated)
         contactArray = appDelegate.fetchAllContacts()
         print("Count \(contactArray.count)")
+        contactTableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
